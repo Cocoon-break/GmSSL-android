a7ad8b8844966aa996162eedbf5c09d038399d6b
wushengping <wushengping@megvii.com>
2020-12-15 13:18:43 +0800

    [add] usage

--- app/src/main/java/com/megvii/gm_android/MainActivity.java
+++ app/src/main/java/com/megvii/gm_android/MainActivity.java
@@ -3,12 +3,23 @@ package com.megvii.gm_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.megvii.gmlib.GmSSL;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "GmSSL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GmSSL gmSSL = new GmSSL();
+        String[] versions = gmSSL.getVersions();
+        for (int i = 0; i < versions.length; i++) {
+            Log.d(TAG, versions[i]);
+        }
+
     }
 }
